ed5b725e0e4fbb84964afa9bf5b444e3484b2e61
Tok <github@zetoken.net>
2020-08-12 15:03:18 +0200

    - new: Support for System.Json.Text in an independent project and nuget package.

---
+++ System.Text.Json.Helpers.UnitTest/JsonHelpersUnitTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using ZTn.System.Text.Json.Helpers;
+
+namespace ZTn.Json.Helpers.UnitTest
+{
+    [TestFixture]
+    public class JsonHelpersUnitTest
+    {
+        private const string IntArrayJson = "[31,32,33,34]";
+        private const string PiObjectJson = "{\"Key\":\"Pi\",\"Value\":3.14159}";
+        private const string PiObjectJsonNetFramework = "{\"Key\":\"Pi\",\"Value\":3.1415899999999999}";
+
+        [Test]
+        public void IntArrayToJson()
+        {
+            var result = new[] { 31, 32, 33, 34 }.ToJson();
+
+            Assert.AreEqual(IntArrayJson, result);
+        }
+
+        [Test]
+        [Platform(Include ="NetCore")]
+        public void KeyValuePairToJson_NetCore()
+        {
+            var result = new KeyValuePair<string, double>("Pi", 3.14159).ToJson();
+
+            Assert.AreEqual(PiObjectJson, result, "Expected to fail with .net 4.x framework (https://github.com/dotnet/runtime/issues/435)");
+        }
+
+        [Test]
+        [Platform(Include ="Net")]
+        public void KeyValuePairToJson_NetFramework()
+        {
+            var result = new KeyValuePair<string, double>("Pi", 3.14159).ToJson();
+
+            Assert.AreEqual(PiObjectJsonNetFramework, result, "Expected to fail with .net core (https://github.com/dotnet/runtime/issues/435)");
+        }
+
+        [Test]
+        [Platform(Include ="NetCore")]
+        public void KeyValuePairToIndentedJson_NetCore()
+        {
+            var result = new KeyValuePair<string, double>("Pi", 3.14159).ToPrettyJson();
+            var expected =
+                "{" + Environment.NewLine +
+                "  \"Key\": \"Pi\"," + Environment.NewLine +
+                "  \"Value\": 3.14159" + Environment.NewLine +
+                "}";
+
+            Assert.AreEqual(expected, result, "Expected to fail with .net 4.x framework (https://github.com/dotnet/runtime/issues/435)");
+        }
+
+        [Test]
+        [Platform(Include ="Net")]
+        public void KeyValuePairToIndentedJson_NetFramework()
+        {
+            var result = new KeyValuePair<string, double>("Pi", 3.14159).ToPrettyJson();
+            var expected =
+                "{" + Environment.NewLine +
+                "  \"Key\": \"Pi\"," + Environment.NewLine +
+                "  \"Value\": 3.1415899999999999" + Environment.NewLine +
+                "}";
+
+            Assert.AreEqual(expected, result, "Expected to fail with .net core (https://github.com/dotnet/runtime/issues/435)");
+        }
+
+        [Test]
+        public void JsonToIntArray()
+        {
+            var result = "[31, 32, 33, 34]".FromJson<int[]>();
+
+            CollectionAssert.AreEqual(new[] { 31, 32, 33, 34 }, result);
+        }
+
+        [Test]
+        public void JsonToKeyValuePair()
+        {
+            var result = PiObjectJson.FromJson<KeyValuePair<string, double>>();
+
+            Assert.AreEqual("Pi", result.Key);
+            Assert.AreEqual(3.14159, result.Value);
+        }
+
+        [Test]
+        public void JsonStreamToKeyValuePair()
+        {
+            KeyValuePair<string, double> result;
+            using (var memoryStream = new MemoryStream(Encoding.Default.GetBytes(PiObjectJson)))
+            {
+                result = memoryStream.ReadAsJson<KeyValuePair<string, double>>();
+                Assert.DoesNotThrow(() => memoryStream.Position = 0);
+            }
+
+            Assert.AreEqual("Pi", result.Key);
+            Assert.AreEqual(3.14159, result.Value);
+        }
+    }
+}
---
+++ System.Text.Json.Helpers/JsonHelpers.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace ZTn.System.Text.Json.Helpers
+{
+    /// <summary>
+    /// Provides some extension methods for convenient use of System.Text.Json serializer.
+    /// </summary>
+    public static class JsonHelpers
+    {
+        /// <summary>
+        /// Creates an instance of type <typeparamref Name="T"/> from a JSON string.
+        /// </summary>
+        /// <typeparam name="T">Target instance type.</typeparam>
+        /// <param name="json">JSON source string.</param>
+        /// <param name="options">JSON options.</param>
+        /// <returns>A new instance of <typeparamref Name="T"/> read from <paramref Name="json"/>.</returns>
+        public static T FromJson<T>(this string json, JsonSerializerOptions options = null)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
+
+            return (T)JsonSerializer.Deserialize(json, typeof(T), options);
+        }
+
+        /// <summary>
+        /// Reads (deserializes) an instance of type <typeparamref Name="T"/> from a JSON stream.
+        /// The stream is safely disposed by the method.
+        /// </summary>
+        /// <typeparam name="T">Target instance type.</typeparam>
+        /// <param name="stream">JSON source stream.</param>
+        /// <param name="options">JSON options.</param>
+        /// <returns>A new instance of <typeparamref Name="T"/> read from <paramref Name="stream"/>.</returns>
+        public static T ReadAsJson<T>(this Stream stream, JsonSerializerOptions options = null)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            return (T)JsonSerializer.DeserializeAsync(stream, typeof(T), options).Result;
+        }
+
+        /// <summary>
+        /// Reads (deserializes) an instance of type <typeparamref Name="T"/> from a JSON file.
+        /// </summary>
+        /// <typeparam name="T">Target instance type.</typeparam>
+        /// <param name="fileName">Path to source JSON file.</param>
+        /// <param name="options">JSON options.</param>
+        /// <returns>A new instance of <typeparamref Name="T"/> read from <paramref Name="fileName"/>.</returns>
+        public static T ReadAsJson<T>(this string fileName, JsonSerializerOptions options = null)
+        {
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                return (T)JsonSerializer.DeserializeAsync(stream, typeof(T), options).Result;
+            }
+        }
+
+        /// <summary>
+        /// Converts (serializes) an instance to a JSON string.
+        /// </summary>
+        /// <param name="instance">Source instance.</param>
+        /// <param name="options">JSON options.</param>
+        public static string ToJson(this object instance, JsonSerializerOptions options = null)
+        {
+            return JsonSerializer.Serialize(instance, options);
+        }
+
+        /// <summary>
+        /// Converts (serializes) an instance to a pretty JSON string (indented).
+        /// </summary>
+        /// <param name="instance">Source instance.</param>
+        /// <param name="options">JSON options.</param>
+        public static string ToPrettyJson(this object instance, JsonSerializerOptions options = null)
+        {
+            return JsonSerializer.Serialize(instance, options.MakeItPretty());
+        }
+
+        /// <summary>
+        /// Writes (serializes) an instance to a stream as JSON.
+        /// </summary>
+        /// <param name="instance">Source instance.</param>
+        /// <param name="stream">Target stream.</param>
+        /// <param name="options">JSON options.</param>
+        public static void WriteAsJson(this object instance, Stream stream, JsonSerializerOptions options = null)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            var jsonOptions = options.MakeItPretty();
+
+            using (var jsonWriter = new Utf8JsonWriter(stream, jsonOptions.ToJsonWriterOptions()))
+            {
+                JsonSerializer.Serialize(jsonWriter, instance, jsonOptions);
+            }
+        }
+
+        /// <summary>
+        /// Writes (serializes) an instance to a JSON file.
+        /// </summary>
+        /// <param name="instance">Source instance.</param>
+        /// <param name="fileName">Path to target JSON file.</param>
+        /// <param name="options">JSON options.</param>
+        public static void WriteAsJson(this object instance, string fileName, JsonSerializerOptions options = null)
+        {
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                instance.WriteAsJson(stream, options);
+            }
+        }
+
+        /// <summary>
+        /// Writes (serializes) an instance to a stream as pretty JSON (indented).
+        /// </summary>
+        /// <param name="instance">Source instance.</param>
+        /// <param name="stream">Target stream.</param>
+        /// <param name="options">JSON options.</param>
+        public static void WriteAsPrettyJson(this object instance, Stream stream, JsonSerializerOptions options = null)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            var jsonOptions = options.MakeItPretty();
+
+            using (var jsonWriter = new Utf8JsonWriter(stream, jsonOptions.ToJsonWriterOptions()))
+            {
+                JsonSerializer.Serialize(jsonWriter, instance, jsonOptions);
+            }
+        }
+
+        /// <summary>
+        /// Writes (serializes) an instance to a pretty JSON file (indented).
+        /// </summary>
+        /// <param name="instance">Source instance.</param>
+        /// <param name="fileName">Path to target JSON file.</param>
+        /// <param name="options">JSON options.</param>
+        public static void WriteAsPrettyJson(this object instance, string fileName, JsonSerializerOptions options = null)
+        {
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                instance.WriteAsPrettyJson(stream, options);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="JsonSerializerOptions"/> or updates an existing one and set <see cref="JsonSerializerOptions.WriteIndented"/> to <c>true</c>.
+        /// </summary>
+        /// <param name="options">JSON options.</param>
+        /// <returns></returns>
+        private static JsonSerializerOptions MakeItPretty(this JsonSerializerOptions options)
+        {
+            var jsonOptions = options ?? new JsonSerializerOptions();
+            jsonOptions.WriteIndented = true;
+
+            return jsonOptions;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="JsonWriterOptions"/> matching the given <see cref="JsonSerializerOptions"/>.<br/>
+        /// From https://docs.microsoft.com/en-us/dotnet/api/system.text.json.jsonserializer.serialize?view=netcore-3.1:
+        /// <quote>The JsonWriterOptions used to create the instance of the Utf8JsonWriter take precedence over the JsonSerializerOptions when they conflict.</quote>
+        /// </summary>
+        /// <param name="options">JSON options.</param>
+        /// <returns></returns>
+        private static JsonWriterOptions ToJsonWriterOptions(this JsonSerializerOptions options)
+        {
+            if (options == null)
+            {
+                return new JsonWriterOptions();
+            }
+
+            return new JsonWriterOptions
+            {
+                Encoder = options.Encoder,
+                Indented = options.WriteIndented
+            };
+        }
+    }
+}
